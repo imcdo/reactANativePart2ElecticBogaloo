a9bc61d54442cf55b2919946601f47a989e5057f
Alexander <alexanderhardinger@gmail.com>
2018-07-14 19:29:35 -0500

    The 'tinder' cards that we will be using

--- src/components/common/TripCard.js
+++ src/components/common/TripCard.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
+const userImage = 'https://i.kym-cdn.com/entries/icons/original/000/016/546/hidethepainharold.jpg'
+const userName = 'omegaLul'
+const userAge = '69'
+const userGender = 'male'
 
 export default class TripCard extends Component {
     render() {
         return(
             <View style={styles.card}>
-            <Text style={styles.header}>Name : {this.props.person.name}</Text>
-                <View style={styles.body}>
-                    <Image source={this.props.person.pic}/>
-                    <View>
-                        <Text>Description</Text>
-                        <Text></Text>
-                    </View>
-                </View>
+                <Image
+                style={{flex:1}}
+                    source={{uri:userImage}}
+                />
+            <View style={{margin:20}}>
+                <Text style={{fontSize:20}}>kek, 69</Text>
+                <Text style={{fontSize:15, color:'darkgrey'}}>fluid</Text>
+            </View>
             </View>
         );
     }
@@ -21,12 +25,15 @@ export default class TripCard extends Component {
 
 const styles =  StyleSheet.create({
     card : {
-        backgroundColor: '#ADD8E6',
+        flex: 1,
+        overflow:'hidden',
+        backgroundColor: 'white',
         alignItems: 'center',
-        flex: 1, 
-        margin : 10,
-        width : '100%'
-        
+        marginTop: 100,
+        marginBottom: 100,
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+        borderRadius: 8
     },
     body : {
         flex : 5,
